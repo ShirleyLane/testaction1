e2b0b80d231fa52ffd0a4a3bd4275d595be15914
ShirleyLane <149216032+ShirleyLane@users.noreply.github.com>
2024-03-03 04:37:13 +0000

    update

---
+++ d1.py
@@ -0,0 +1,41 @@
+import requests
+import sys
+import os
+
+def get_last_friday():
+  from datetime import datetime, timedelta
+                                                              
+  #today = datetime.today() - timedelta(days=2)
+  today = datetime.today()
+  #print(today)
+                                                              
+  weekday = today.weekday()
+                                                              
+  if weekday == 4:
+    last_friday = today - timedelta(days=7)
+  else:
+    days_to_last_friday = (weekday - 4 + 7) % 7
+    last_friday = today - timedelta(days=days_to_last_friday)
+                                                              
+  last_friday_formatted = last_friday.strftime("%Y%m%d")
+  return(last_friday_formatted)
+
+
+if __name__ == "__main__":
+  date = get_last_friday()
+  filename = "tdcc_data_%s.csv" % date
+  if not os.path.exists(filename):
+    url = "https://opendata.tdcc.com.tw/getOD.ashx?id=1-5"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 14_3_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.3.1 Safari/605.1.15"
+    }
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+      with open(filename, "wb") as f:
+        f.write(response.content)
+        print("finished", file=sys.stderr)
+    else:
+      print("failed", file=sys.stderr)
+  else:
+    print("file already exist", file=sys.stderr)
